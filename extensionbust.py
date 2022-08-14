d8eee272edca497eff64ec699f196b76c16b3fae
MayankPal <110147408+MayankpalCS@users.noreply.github.com>
2022-08-14 14:12:16 +0530

    Create extensionbust.py

---
+++ extensionbust.py
@@ -0,0 +1,39 @@
+import requests
+import queue
+import threading
+import sys
+
+host = sys.argv[1]
+threads = int(sys.argv[2])
+ext = sys.argv[3]
+try:
+    requests.get(host)
+except Exception as e:
+    print('Host resolution error')
+    exit()
+wordlist=open('directorybust.txt', 'r')
+q = queue.Queue()
+
+
+def busting(thread, q):
+    while True:
+        url = q.get()
+
+        response = requests.get(url,allow_redirects=False)
+        if response.status_code == 200:
+            print(f"[+]{url} directory exists")
+        q.task_done()
+for words in wordlist.read().splitlines():
+    if not ext:
+        url = host + '/' + words
+    else:
+        url = host + '/' + words + ext
+    q.put(url)
+for i in range(threads):
+    t = threading.Thread(target=busting,args=(i,q))
+    t.daemon = True
+    t.start()
+
+
+
+
