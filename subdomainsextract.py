80ead2052f97aeb1c9a5933067d971d5d283400d
MayankPal <110147408+MayankpalCS@users.noreply.github.com>
2022-08-15 14:42:39 +0530

    Update subdomainsextract.py

--- subdomainsextract.py
+++ subdomainsextract.py
@@ -1,1 +1,64 @@
+import time
 
+import requests
+import queue
+import threading
+import sys
+import argparse
+from colorama import init, Fore
+
+init()
+red = Fore.RED
+green = Fore.GREEN
+yellow = Fore.YELLOW
+reset = Fore.RESET
+def banner():
+    print(f"""{green}
+           |------|  ------- |-----|     |-----| |     | |--------| -------- |----- |----|
+           |      |     |    |     |     |     | |     | |             |     |      |    |
+           |      |     |    |-----|     |-----| |     | |-------|     |     |----- |----|
+           |      |     |    ||          |     | |     |         |     |     |      ||
+           |------|  ------- |  |        |-----| |-----| |-------|     |     |----- | |
+            @ developed by mayank |                                                     |
+                           """)
+
+
+banner()
+
+argparse = argparse.ArgumentParser(description="Use this tool to bust directories",
+                                   usage="python3" + sys.argv[0] + "-u [url] -t [no_of+threads] -d directorybust.txt ")
+argparse.add_argument("-h", "--host", help="Enter the domain name on which you want to perform directory busting",
+                      required=True)
+argparse.add_argument("-t", "--threads", help="no of threads", required=True)
+argparse.add_argument("-d", "--direc", help="Enter the name of wordlist one is provided,Fell free to name that",
+                      required=True)
+args = argparse.parse_args()  # parsed the values
+host = args.host  # above we enteres --domain will be fetched hhere
+threads = int(args.threads)
+wordlist = args.direc
+q = queue.Queue()
+print(f"{green}[+]Seaching for subdomains")
+wordlist = open(wordlist, 'r')
+for words in wordlist.read().splitlines():
+    url = 'https://' + words + '.' + host
+    q.put(url)
+
+
+def fuckyeah(thread, q):
+    while True:
+        url = q.get()
+        try:
+            res = requests.get(url, allow_redirects=False)
+            if res.status_code != 404:
+                print(f"{green}[+]{url} is there")
+        except Exception as e:
+            print("Ctrl+C to exit search is complete")
+        q.task_done()
+
+
+for i in range(threads):
+
+    time.sleep(1)
+    t = threading.Thread(target=fuckyeah, args=(i, q))
+    t.daemon = True
+    t.start()
